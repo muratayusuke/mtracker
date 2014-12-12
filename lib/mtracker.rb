d13461edba667c48524bf90a5271f829dc892e34
Yukio Goto <yukio.goto@mail.rakuten.com>
2014-12-12 10:23:52 +0800

    fix output bug

--- lib/mtracker.rb
+++ lib/mtracker.rb
@@ -4,9 +4,9 @@ require 'mtracker/version'
 module Mtracker
   def track(label)
     start_time = Time.now
-    puts "[start] #{label}"
+    put_track_info "[start] #{label}"
     yield
-    puts "[end] #{label} (#{Time.now - start_time} sec)"
+    put_track_info "[end] #{label} (#{Time.now - start_time} sec)"
   end
 
   def put_track_info(msg)
